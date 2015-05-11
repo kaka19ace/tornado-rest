5e120c911b94a0ae49762976a5e75df5fc08eb03
kaixiang zhong <xiang.ace@gmail.com>
2015-05-11 15:00:52 +0800

    update rest

--- pyrestful/rest.py
+++ pyrestful/rest.py
@@ -16,16 +16,29 @@
 
 # -*- coding: utf-8 -*-
 
+import sys
+PY2 = (sys.version_info[0] == 2)
+import traceback
+
 import tornado.ioloop
 import tornado.concurrent
+from tornado import gen
 import tornado.web
 import tornado.wsgi
 import xml.dom.minidom
 import inspect
 import re
 import json
+import functools
 
 from pyrestful import mediatypes, types
+
+
+def _list_from_filter(f, iterable):
+    if PY2:
+        return filter(f, iterable)
+    else:
+        return list(filter(f, iterable))
 
 
 class PyRestfulException(Exception):
@@ -37,12 +50,13 @@ class PyRestfulException(Exception):
         return repr(self.message)
 
 
-def config(func,method,**kwparams):
+def config(func, method,**kwparams):
     """ Decorator config function """
     path     = None
     produces = None
     consumes = None
     types    = None
+    is_coroutine = False
 
     if len(kwparams):
         path = kwparams['_path']
@@ -54,23 +68,27 @@ def config(func,method,**kwparams):
             consumes = kwparams['_consumes']
         if '_types' in kwparams:
             types = kwparams['_types']
+        if kwparams.get('_coroutine', False) is True:
+            is_coroutine = True
 
     def operation(*args,**kwargs):
         return func(*args,**kwargs)
 
-    operation.func_name       = func.func_name
+    operation.func_name = func.__name__
+    operation.__name__       = func.__name__
     operation._func_params    = inspect.getargspec(func).args[1:]
     operation._types          = types or [str]*len(operation._func_params)
-    operation._service_name   = re.findall(r"(?<=/)\w+",path)
-    operation._service_params = re.findall(r"(?<={)\w+",path)
+    operation._service_name   = re.findall(r"(?<=/)\w+", path)
+    operation._service_params = re.findall(r"(?<={)\w+", path)
     operation._method         = method
     operation._produces       = produces
     operation._consumes       = consumes
-    operation._query_params   = re.findall(r"(?<=<)\w+",path)
+    operation._query_params   = re.findall(r"(?<=<)\w+", path)
     operation._path           = path
+    operation._is_coroutine = is_coroutine
 
     if not operation._produces in [mediatypes.APPLICATION_JSON,mediatypes.APPLICATION_XML,mediatypes.TEXT_XML]:
-        raise PyRestfulException("The media type used do not exist : "+operation.func_name)
+        raise PyRestfulException("The media type used do not exist : " + operation.__name__)
 
     return operation
 
@@ -104,6 +122,9 @@ def delete(*params, **kwparams):
 
 
 class RestHandler(tornado.web.RequestHandler):
+    def __init__(self, *args, **kwargs):
+        super(RestHandler, self).__init__(*args, **kwargs)
+
     def get(self):
         """ Executes get method """
         self._exe('GET')
@@ -124,53 +145,62 @@ class RestHandler(tornado.web.RequestHandler):
         """ Executes the python function for the Rest Service """
         request_path = self.request.path
         path = request_path.split('/')
-        services_and_params = filter(lambda x: x!='',path)
+        services_and_params = _list_from_filter(lambda x: x != '', path)
 
-        # Get all funcion names configured in the class RestHandler
-        functions    = filter(lambda op: hasattr(getattr(self,op),'_service_name') == True and inspect.ismethod(getattr(self,op)) == True, dir(self))
+        # Get all function names configured in the class RestHandler
+        functions = _list_from_filter(
+            lambda op: hasattr(getattr(self, op), '_service_name') is True and inspect.ismethod(getattr(self, op)) is True,
+            dir(self)
+        )
         # Get all http methods configured in the class RestHandler
-        http_methods = map(lambda op: getattr(getattr(self,op),'_method'), functions)
+        http_methods = map(lambda op: getattr(getattr(self, op), '_method'), functions)
 
         if method not in http_methods:
-            raise tornado.web.HTTPError(405,'The service not have %s verb'%method)
+            raise tornado.web.HTTPError(405, 'The service not have %s verb'%method)
 
-        for operation in map(lambda op: getattr(self,op), functions):
+        for operation in map(lambda op: getattr(self, op), functions):
             service_name          = getattr(operation,"_service_name")
             service_params        = getattr(operation,"_service_params")
             # If the _types is not specified, assumes str types for the params
-            params_types          = getattr(operation,"_types") or [str]*len(service_params)
-            params_types          = map(lambda x,y : y if x is None else x, params_types, [str]*len(service_params))
-            produces              = getattr(operation,"_produces")
-            services_from_request = filter(lambda x: x in path,service_name)
-            query_params          = getattr(operation,"_query_params")
+            params_types          = getattr(operation, "_types") or [str]*len(service_params)
+            params_types          = list(map(lambda x,y : y if x is None else x, params_types, [str]*len(service_params)))
+            produces              = getattr(operation, "_produces")
+            services_from_request = _list_from_filter(
+                lambda x: x in path,
+                service_name
+            )
+            query_params          = getattr(operation, "_query_params")
 
             if operation._method == self.request.method and service_name == services_from_request and len(service_params) + len(service_name) == len(services_and_params):
                 try:
-                    params_values = self._find_params_value_of_url(service_name,request_path) + self._find_params_value_of_arguments(operation)
+                    params_values = self._find_params_value_of_url(service_name, request_path) + self._find_params_value_of_arguments(operation)
                     p_values      = self._convert_params_values(params_values, params_types)
-                    response      = operation(*p_values)
+
+                    response = operation(*p_values)
 
                     if response is None:
                         return
 
-                    self.set_header("Content-Type",produces)
+                    self.set_header("Content-Type", produces)
 
-                    if produces == mediatypes.APPLICATION_JSON and isinstance(response,dict):
+                    if produces == mediatypes.APPLICATION_JSON and isinstance(response, dict):
                         self.write(response)
                         self.finish()
-                    elif produces == mediatypes.APPLICATION_JSON and isinstance(response,list):
+                    elif produces == mediatypes.APPLICATION_JSON and isinstance(response, list):
                         self.write(json.dumps(response))
                         self.finish()
                     elif produces in [mediatypes.APPLICATION_XML,mediatypes.TEXT_XML] and isinstance(response,xml.dom.minidom.Document):
                         self.write(response.toxml())
                         self.finish()
-                    elif isinstance(response, tornado.concurrent.Future):
-                        "Do Nothing when using tornado.gen.coroutine"
-                        pass
                     else:
-                        self.gen_http_error(500,"Internal Server Error : response is not %s document"%produces)
-                except Exception as detail:
-                    self.gen_http_error(500,"Internal Server Error : %s"%detail)
+                        print("[ERROR] Internal Server Error : response is not {0} document".format(produces))
+                        self.gen_http_error(500, "Internal Server Error : response is not %s document" % produces)
+                except tornado.web.HTTPError as e:
+                    traceback.print_exc()
+                    self.gen_http_error(e.status_code, "HTTP Error")
+                except Exception as e:
+                    self.gen_http_error(500, "Internal Server Error")
+                    raise Exception().with_traceback(e.__traceback__)
 
     def _find_params_value_of_url(self,services,url):
         """ Find the values of path params """
@@ -201,18 +231,21 @@ class RestHandler(tornado.web.RequestHandler):
         return values
 
     def _convert_params_values(self, values_list, params_types):
-        """ Converts the values to the specifics types """
+        """ Converts the values to the specifics types
+        :param values_list:
+        :param params_types:
+        """
         values = list()
         i = 0
         for v in values_list:
             if v is not None:
-                values.append(types.convert(v,params_types[i]))
+                values.append(types.convert(v, params_types[i]))
             else:
                 values.append(v)
             i += 1
         return values
 
-    def gen_http_error(self,status,msg):
+    def gen_http_error(self, status, msg):
         """ Generates the custom HTTP error """
         self.clear()
         self.set_status(status)
@@ -220,34 +253,34 @@ class RestHandler(tornado.web.RequestHandler):
         self.finish()
 
     @classmethod
-    def get_services(self):
+    def get_services(cls):
         """ Generates the resources (uri) to deploy the Rest Services """
         services = []
-        for f in dir(self):
-            o = getattr(self,f)
-            if callable(o) and hasattr(o,'_service_name'):
-                services.append(getattr(o,'_service_name'))
+        for f in dir(cls):
+            o = getattr(cls,f)
+            if callable(o) and hasattr(o, '_service_name'):
+                services.append(getattr(o, '_service_name'))
         return services
 
     @classmethod
-    def get_paths(self):
+    def get_paths(cls):
         """ Generates the resources from path (uri) to deploy the Rest Services """
         paths = []
-        for f in dir(self):
-            o = getattr(self,f)
-            if callable(o) and hasattr(o,'_path'):
-                paths.append(getattr(o,'_path'))
+        for f in dir(cls):
+            o = getattr(cls, f)
+            if callable(o) and hasattr(o, '_path'):
+                paths.append(getattr(o, '_path'))
         return paths
 
     @classmethod
-    def get_handlers(self):
+    def get_handlers(cls):
         """ Gets a list with (path, handler) """
         svs = []
-        paths = self.get_paths()
+        paths = cls.get_paths()
         for p in paths:
-            s = re.sub(r"(?<={)\w+}",".*",p).replace("{","")
-            o = re.sub(r"(?<=<)\w+","",s).replace("<","").replace(">","").replace("&","").replace("?","")
-            svs.append((o,self))
+            s = re.sub(r"(?<={)\w+}", ".*", p).replace("{", "")
+            o = re.sub(r"(?<=<)\w+", "", s).replace("<", "").replace(">", "").replace("&", "").replace("?", "")
+            svs.append((o, cls))
 
         return svs
 
